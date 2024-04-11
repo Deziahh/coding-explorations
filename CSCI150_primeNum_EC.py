42f02e908ae5d3176e9d1ad333786a7fdc370949
Deziah Gayle <deziahgayle@gmail.com>
2024-04-11 17:10:09 -0400

    finished prime number project

--- CSCI150_primeNum_EC.py
+++ CSCI150_primeNum_EC.py
@@ -1,2 +1,12 @@
 def primeNumbers(n):
-    
+    allNum = [2, 3, 5, 7]
+    for i in range(3,n+1):
+        if i%2 == 0 or i%3 == 0 or i%5 == 0 or i%7 == 0: 
+            continue
+        else:
+            allNum.append(i)
+         
+    return allNum
+
+a = primeNumbers(101)
+print(a)
